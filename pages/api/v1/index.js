b490b32e02a78693c5e42fb13ae4057b255a0910
FLOpeSS <sneto4413@gmail.com>
2024-04-25 20:52:52 -0300

    testing and launching apis

---
+++ pages/api/v1/index.js
@@ -0,0 +1,5 @@
+function status(req, res) {
+    res.status(200).json({key: "successfull request to v1"})
+}
+
+export default status
---
+++ pages/api/v2/index.js
@@ -0,0 +1,6 @@
+function status(req, res) {
+    res.status(200).json({key: "successfull request to v2"})
+}
+
+
+export default status;
