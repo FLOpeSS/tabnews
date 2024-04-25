d45d35be272484f1e6e65721e2fa4f5cb5006e50
FLOpeSS <sneto4413@gmail.com>
2024-04-24 23:47:08 -0300

    adding an dog image and styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import { Analytics } from "@vercel/analytics/react"
+import Image from 'next/image'
+
+const styles = {
+    texting : {
+        textAlign: 'center',
+        marginTop: '50px'
+    },
+    image: {
+        display: 'block',
+        margin: 'auto',
+        maxWidth: '100%', // Ensure image does not exceed container width
+        maxHeight: '100%', // Ensure image does not exceed container height
+    },
+
+}
 
 function Home() {
     return (
-        <body>
-            <h1>In construction by SebasOnRails</h1>
+        <div>
+            <h1 style={styles.texting}>In construction by SebasOnRails</h1>
+            <Image
+              src="/dog.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author's dog"
+              style={styles.image}
+            />
             <Analytics />
-        </body>
-
-    )
+        </div>
+    );
 }
+
 
 export default Home;
 
